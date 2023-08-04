091087e7a8114f593a3db0ac550c64c557e2f321
kzlucky <samuelzinhog.buenobispo9@gmail.com>
2023-08-04 04:43:51 -0300

    Second

--- config.php
+++ config.php
@@ -1,10 +1,6 @@
 <?php
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASS','');
-    define('BASE', 'cadastro');
-
-
-    $conn = new mysqli(HOST, USER, PASS, BASE);
-
-
+  $mysqli = mysqli_init();
+  $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+  $mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
+  $mysqli->close();
+?>
